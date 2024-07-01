d520bee62b81876e451b7c5e1b04b8f8bbd08666
VPolozhay <vlados988111@gmail.com>
2024-07-01 14:49:35 +0300

    optimization

--- Sources/NetworkInfo/Private/WiFiInfo.swift
+++ Sources/NetworkInfo/Private/WiFiInfo.swift
@@ -3,13 +3,16 @@ import Foundation
 
 final class WiFiInfo {
     func getCurrentSSID() -> String? {
-        if let interfaces = CNCopySupportedInterfaces() as? [String] {
-            for interface in interfaces {
-                if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as CFString) as NSDictionary? {
-                    return interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
-                }
+        guard let interfaces = CNCopySupportedInterfaces() as? [String] else {
+            return nil
+        }
+        
+        for interface in interfaces {
+            if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as CFString) as NSDictionary? {
+                return interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
             }
         }
+        
         return nil
     }
 }
